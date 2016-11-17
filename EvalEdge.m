3464631beb1ae851023e5ce3eb974ef05f06723b
Yu Liu <y.liu@liacs.leidenuniv.nl>
2016-11-17 16:07:06 +0100

    Update EvalEdge.m

--- EvalEdge.m
+++ EvalEdge.m
@@ -7,8 +7,6 @@ addpath(genpath('../edges-master/'));
 %% Simple version
 tic;
 resDir = './RDS_SE_NMS/';
-%resDir = '../new-hed-master/examples/hed/Results/Reproduce5/hed_BSDS_Pretrained_NMS/';
-%resDir = '~/Datasets/BSDS500/BSDS500/data/groundTruth/test_3annotators_400PNG_dilate/';
 fprintf('%s\n',resDir);
 gtDir = '~/Datasets/BSDS500/BSDS500/data/groundTruth/test';
 edgesEvalDir('resDir',resDir,'gtDir',gtDir, 'thin', 1, 'pDistr',{{'type','parfor'}},'maxDist',0.0075);
